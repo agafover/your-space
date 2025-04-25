b8995833c9341b8a9fc26d185658e9361c253bf9
agafover <agafover@cvut.cz>
2025-04-25 16:57:22 +0200

    dark theme in contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,18 +1,21 @@
 import { Mail } from 'lucide-react'
+
 function Contacts() {
   return (
     <div className="max-w-xl mx-auto py-10 px-4 text-center">
-      <h1 className="text-3xl font-bold text-brand-dark mb-6">Контакты</h1>
-      <p className="text-lg text-gray-700 mb-4">
-        Если хочешь задать вопрос, предложить идею или узнать больше о сообществе —
-        напиши нам 💌
+      <h1 className="text-3xl font-bold text-brand-dark dark:text-brand-light mb-6">
+        Контакты
+      </h1>
+
+      <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
+        Если хочешь задать вопрос, предложить идею или узнать больше о сообществе — напиши нам!
       </p>
 
       <a
         href="mailto:community.yourspace@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="inline-flex items-center gap-2 border border-brand-dark text-brand-dark px-4 py-2 rounded-xl hover:bg-brand-dark hover:text-white transition"
+        className="inline-flex items-center gap-2 border border-brand-dark dark:border-brand-light text-brand-dark dark:text-brand-light px-4 py-2 rounded-xl hover:bg-brand-dark hover:text-white dark:hover:bg-brand-light dark:hover:text-brand-dark transition"
       >
         <Mail /> Связаться с нами
       </a>
